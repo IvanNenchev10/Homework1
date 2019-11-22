d9fe465fee321e9616842bfa0aee9e39c47df40c
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-11-22 12:39:01 +0200

    Add files via upload

---
+++ Задача1.2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Задача2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name, colour, gender;
+            int age;
+            Console.WriteLine("Enter name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter age:");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter colour of the eyes");
+            colour = Console.ReadLine();
+            Console.WriteLine("Enter gender:");
+            gender = Console.ReadLine();
+            Console.WriteLine("Name:{0}", name);
+            Console.WriteLine("Age:{0}", age);
+            Console.WriteLine("Colour:{0}", colour);
+            Console.WriteLine("Gender:{0}", gender);
+        }
+    }
+}
