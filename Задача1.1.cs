09c8c4f0fbbfc8a47c4297eb9f5aeded945eae4b
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-11-22 12:38:13 +0200

    Add files via upload

---
+++ Задача1.1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Задача1
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime today = DateTime.Today;
+            Console.WriteLine("Today:{0}", today);
+        }
+    }
+}
